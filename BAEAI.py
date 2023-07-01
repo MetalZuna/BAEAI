a67f9858feefa8e0b9de144f0eecc1254ca2560b
MetalZuna <didarkaramsingh@gmail.com>
2023-06-30 23:37:04 -0400

    made some change

--- BAEAI.py
+++ BAEAI.py
@@ -4,6 +4,11 @@ from flask import Flask
 from models import init_db, db
 from routes import register_routes
 from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
 
 
 app = Flask(__name__)
@@ -13,10 +18,21 @@ db.init_app(app)
 
 # Initialize language model
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, temperature=0)
+chat=ChatOpenAI()
+
+messages = [
+    SystemMessage(content="You are a helpful assistant that translates suggests a dinner plans."),
+    HumanMessage(content="suggest a Punjabi vegetarian 5 course meal for 5 people.")
+]
+result=chat(messages)
+
+
+print(result)
+
+
 
 # Register routes
-register_routes(app, db, llm)
+register_routes(app, db, chat)
 
 if __name__ == '__main__':
     with app.app_context():
